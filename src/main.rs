0e0e776abc9dbc0bd38a35ea0d7002dba3c14eac
François <francois.mockers@vleue.com>
2024-11-08 23:54:03 +0100

    back to the start

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,3 @@
-use bevy::prelude::*;
-
-mod menu;
-mod splash;
-
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "Bevy Workshop".into(),
-                canvas: Some("#game".into()),
-                ..default()
-            }),
-            ..default()
-        }))
-        .init_state::<GameState>()
-        .enable_state_scoped_entities::<GameState>()
-        .add_plugins((splash::splash_plugin, menu::menu_plugin))
-        .run();
-}
-
-#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash, States, Default)]
-enum GameState {
-    #[default]
-    Splash,
-    Menu,
-    Game,
+    println!("Hello, world!");
 }
